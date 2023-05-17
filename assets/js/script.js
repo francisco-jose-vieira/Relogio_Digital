0e7493554ced899534d2bb958f00a5f788c75484
francisco-jose-vieira <franciscojose2703@gmail.com>
2023-05-17 12:23:41 -0300

    Projeto finalizado

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+/* linhas responsáveis por pegar o valor da tag span com seus devidos id's */
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    /* if's reponsaveis por colocar 0 antes dos números */
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = s
+})
